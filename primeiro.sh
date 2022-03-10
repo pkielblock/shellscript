083a239a1294975f74b98c289f0d1ac52d009ee2
Pedro Kielblock <p.kielblock123@gmail.com>
2022-03-10 15:10:36 -0300

    Add files via upload

---
+++ primeiro.sh
@@ -0,0 +1,3 @@
+clear
+
+echo "Hello World"
---
+++ segundo.sh
@@ -0,0 +1,14 @@
+clear
+
+echo "Entre seu nome:"
+read nome
+
+echo "Entre sua Idade:"
+read idade
+
+echo "Pressione algo para continuar..."
+read algo
+
+clear
+
+echo "Seu nome é: $nome, você tem $idade anos."
